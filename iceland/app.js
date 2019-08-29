5dd030ce1865c7a4cad3901038c62606fd917bf0
Si3ver <izhouwl@163.com>
2019-08-30 02:47:16 +0800

    路由系统设计

--- iceland/app.js
+++ iceland/app.js
@@ -1,19 +1,7 @@
 const Koa = require('koa')
+const InitManager = require('./core/init')
 
 const app = new Koa()
-
-// 注册中间件
-app.use(async (ctx, next) => {
-  await next()
-  const r = ctx.r
-  console.log(r)
-})
-
-app.use(async (ctx, next)=>{
-  const axios = require('axios')
-  const res = await axios.get('http://baidu.com')
-  ctx.r = res.data
-  await next()
-})
+InitManager.initCore(app)
 
 app.listen(3000)
--- iceland/docs/NOTES.md
+++ iceland/docs/NOTES.md
@@ -9,3 +9,12 @@
   + 为了保证执行顺序为‘洋葱模型’，回调函数加`async`，`next()`加`await`
 + 函数加了 `async` 关键字后，`return` 的内容会转换成 `Promise`
 + `await` 必须配合 `async`使用，`await` 会阻塞后面的异步执行，从而实现同步执行。
+
+## 路由系统
+
++ `koa-router`
++ API分级，版本兼容
++ 避免循环引用~
++ `nodemon`实现自动重启 `node` 运行环境
++ 拓展：牛逼的NPM包 `require-directory` 从目录导入一系列模块~  -> 方便自动注册路由！
++ 初始化管理器 与 `Process.cwd`
